e0612e26b656c629247d05c5db9ce6751490886d
Anthony Jung <junganthonyy@gmail.com>
2020-01-28 12:56:06 -0800

    Fix tests

--- database/models.js
+++ database/models.js
@@ -3,5 +3,8 @@ const models = require('./index');
 module.exports = {
   getAll: () => {
     return models.find();
+  },
+  addItems: (items) => {
+    return models.create(items);
   }
 };
--- spec/database.test.js
+++ spec/database.test.js
@@ -1,23 +1,48 @@
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 const models = require('../database/models.js');
 
-describe('insert', () => {
-  let connection;
-  let db;
-  const mongoURI = 'mongodb://localhost/rei';
+describe('database', () => {
+  var db;
+  
+  beforeAll(async () => {
+    const mongoURI = 'mongodb://localhost/rei-test';
+    db = await mongoose.connect(mongoURI, { useNewUrlParser: true });
+  });
 
-  // beforeAll(async () => {
-  //   connection = await mongoose.connect(mongoURI);
-  // });
+  afterEach(async () => {
+    await mongoose.connection.collections['searchitems'].deleteMany();
+  });
 
   afterAll(async () => {
-    await mongoose.connection.close();
+    await mongoose.connection.close(true)
+      .then(() => console.log('closed'))
+      .catch(err => console.error(err));
   });
 
-  it ('should get all the items', async () => {
-    await models.getAll()
-      .then(dbResponse => {
-        expect(dbResponse.length).toBe(100);
+  test ('should get all the items', () => {
+    var sampleSearches = [{ id: 1, name: "testItem1" }, { id: 2, name: "testItem2" }, { id: 3, name: "testItem3" }];
+
+    return models.addItems(sampleSearches)
+      .then(() => {
+        return models.getAll();
       })
+      .then(dbResponse => {
+        expect(dbResponse.length).toBe(3);
+      });
+  });
+  
+  test ('adds a search entry', () => {
+    return models.addItems({id: 1000, name: "test_item"})
+    .then(() => {
+      return models.getAll();
+    })
+    .then(dbResponse => {
+      expect(dbResponse[0]).toEqual(expect.objectContaining({
+        id: expect.any(Number),
+        name: expect.any(String)
+      }));
+    });
   });
 });
+
