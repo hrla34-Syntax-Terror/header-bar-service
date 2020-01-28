8f4ce2d25fa9e85171bf1237f2621d80172d3fd7
Anthony Jung <junganthonyy@gmail.com>
2020-01-27 23:16:25 -0800

    Add Route to server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
+const router = require('./router.js');
 
 const app = express();
 const port = 3001;
@@ -10,5 +11,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 app.use(cors());
+app.use('/api', router);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
