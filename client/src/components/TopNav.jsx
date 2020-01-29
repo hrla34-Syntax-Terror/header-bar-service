7537490bc492919fbadea1ee3063c50ac3b5b433
Anthony Jung <junganthonyy@gmail.com>
2020-01-29 12:17:29 -0800

    Started on topnav

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -2,19 +2,17 @@ import React from 'react';
 
 var TopNav = (props) => {
   return (
-    <div>
-      <ul id="TopNav">
-        <li>SHOP REI</li>
-        <li>REI OUTLET</li>
-        <li>USED GEAR</li>
-        <li>REI ADVENTURES</li>
-        <li>CLASSES &amp; EVENTS</li>
-        <li>EXPERT ADVICE</li>
-        <li>CO-OP JOURNAL</li>
-        <li>CONVERSATIONS</li>
-        <li>CAMPING PROJECT</li>
-      </ul>
-    </div>
+    <ul id="TopNavAJ">
+      <li>SHOP REI</li>
+      <li>REI OUTLET</li>
+      <li>USED GEAR</li>
+      <li>REI ADVENTURES</li>
+      <li>CLASSES &amp; EVENTS</li>
+      <li>EXPERT ADVICE</li>
+      <li>CO-OP JOURNAL</li>
+      <li>CONVERSATIONS</li>
+      <li>CAMPING PROJECT</li>
+    </ul>
   );
 };
 
