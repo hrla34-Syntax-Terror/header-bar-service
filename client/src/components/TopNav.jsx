6b0f74be0fd84d98edee60a9e83e5b373ae44beb
Anthony Jung <junganthonyy@gmail.com>
2020-01-30 11:48:30 -0800

    Complete Top-Nav-Bar

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 
 var TopNav = (props) => {
   return (
-    <div id="AJnavwrapper">
-      <ul id="AJtopnav">
-        <li className="item" id="selected">SHOP REI</li>
-        <li className="item">REI OUTLET</li>
-        <li className="item">USED GEAR</li>
-        <li className="item">REI ADVENTURES</li>
-        <li className="item">CLASSES &amp; EVENTS</li>
-        <li className="item">EXPERT ADVICE</li>
-        <li className="item">CO-OP JOURNAL</li>
-        <li className="item">CONVERSATIONS</li>
-        <li className="item">CAMPING PROJECT</li>
-      </ul>
+    <div id="AJnavwrapperbackground">
+      <div id="AJnavwrapper">
+        <ul id="AJtopnav">
+          <li className="item" id="selected">SHOP REI</li>
+          <li className="item">REI OUTLET</li>
+          <li className="item">USED GEAR</li>
+          <li className="item">REI ADVENTURES</li>
+          <li className="item">CLASSES &amp; EVENTS</li>
+          <li className="item">EXPERT ADVICE</li>
+          <li className="item">CO-OP JOURNAL</li>
+          <li className="item">CONVERSATIONS</li>
+          <li className="item">CAMPING PROJECT</li>
+        </ul>
+      </div>
     </div>
   );
 };
