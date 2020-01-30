98ebee8aca8d4612a0cfcbd20a02b97a3aced721
Anthony Jung <junganthonyy@gmail.com>
2020-01-30 10:59:39 -0800

    Start on CSS

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 var TopNav = (props) => {
   return (
-    <div id="navwraperAJ">
-      <ul id="TopNavAJ">
-        <li>SHOP REI</li>
-        <li>REI OUTLET</li>
-        <li>USED GEAR</li>
-        <li>REI ADVENTURES</li>
-        <li>CLASSES &amp; EVENTS</li>
-        <li>EXPERT ADVICE</li>
-        <li>CO-OP JOURNAL</li>
-        <li>CONVERSATIONS</li>
-        <li>CAMPING PROJECT</li>
+    <div id="AJnavwrapper">
+      <ul id="AJtopnav">
+        <li className="item" id="selected">SHOP REI</li>
+        <li className="item">REI OUTLET</li>
+        <li className="item">USED GEAR</li>
+        <li className="item">REI ADVENTURES</li>
+        <li className="item">CLASSES &amp; EVENTS</li>
+        <li className="item">EXPERT ADVICE</li>
+        <li className="item">CO-OP JOURNAL</li>
+        <li className="item">CONVERSATIONS</li>
+        <li className="item">CAMPING PROJECT</li>
       </ul>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const router = require('./router.js');
+const path = require('path');
 
 const app = express();
 const port = 3001;
@@ -12,5 +13,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 app.use(cors());
 app.use('/api', router);
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(port, () => console.log(`\u001b[1;34m Server listening on port ${port}`));
