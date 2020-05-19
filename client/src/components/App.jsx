7217d8b8e2093760aad2aaca23765cdff6c67a4f
Anthony Jung <junganthonyy@gmail.com>
2020-05-19 11:53:55 -0700

    Change to Functional Component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,23 +2,11 @@ import React from 'react';
 import TopNav from './TopNav.jsx';
 import MainHeader from './MainHeader.jsx';
 
-class App extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  render() {
-    return (
-      <header>
-        <TopNav></TopNav>
-        <MainHeader></MainHeader>
-
-      </header>
-    );
-  }
-}
+const App = () => (
+  <header>
+    <TopNav></TopNav>
+    <MainHeader></MainHeader>
+  </header>
+);
 
 export default App;
