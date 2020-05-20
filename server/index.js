420a1c5c23c30512a6916de083de9bdd7c9f3c29
Anthony Jung <junganthonyy@gmail.com>
2020-05-19 22:06:58 -0700

    Adding Server Route for Favicon

--- server/index.js
+++ server/index.js
@@ -15,4 +15,9 @@ app.use(cors());
 app.use('/api', router);
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+app.get('/favicon.ico', (req, res) => {
+  res.status(200);
+  res.send();
+});
+
 app.listen(port, () => console.log(`\u001b[1;34mServer listening on port ${port}\u001B[37m`));
