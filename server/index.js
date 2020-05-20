c7c74546cffbba8dbb931382796ddaca130a0924
Anthony Jung <junganthonyy@gmail.com>
2020-05-19 22:17:06 -0700

    Adding Proper Favicon Middleware and File

--- server/index.js
+++ server/index.js
@@ -4,20 +4,17 @@ const morgan = require('morgan');
 const cors = require('cors');
 const router = require('./router.js');
 const path = require('path');
+var favicon = require('serve-favicon')  
 
 const app = express();
 const port = 3001;
 
+app.use(favicon(path.join(__dirname, '../client/dist', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 app.use(cors());
 app.use('/api', router);
 app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.get('/favicon.ico', (req, res) => {
-  res.status(200);
-  res.send();
-});
 
 app.listen(port, () => console.log(`\u001b[1;34mServer listening on port ${port}\u001B[37m`));
