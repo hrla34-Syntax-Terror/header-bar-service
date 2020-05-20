01ac19b5c63fa31523ac90c091d2ca0e89d1d4a1
Anthony Jung <junganthonyy@gmail.com>
2020-05-19 20:15:47 -0700

    Commenting Out Database

--- database/index.js
+++ database/index.js
@@ -4,11 +4,15 @@ mongoose.Promise = global.Promise;
 // const mongoURI = 'mongodb://3.101.6.196:27017/rei';
 const mongoURI = 'mongodb://localhost:27017/rei';
 
-mongoose.set('useCreateIndex', true);
+/**
+ * This is the code for the database connection
+ * This has been commented out for easy deployment
+ */
+// mongoose.set('useCreateIndex', true);
 
-var db = mongoose.connect(mongoURI, {useNewUrlParser: true})
-  .then(() => console.log(`\u001b[1;35mMongoDB Connected: ${mongoURI}\u001B[37m`));
+// var db = mongoose.connect(mongoURI, {useNewUrlParser: true})
+//   .then(() => console.log(`\u001b[1;35mMongoDB Connected: ${mongoURI}\u001B[37m`));
 
-var searchModel = mongoose.model('searchitem', searchSchema);
-
+// var searchModel = mongoose.model('searchitem', searchSchema);
+var searchModel = null;
 module.exports = searchModel;
